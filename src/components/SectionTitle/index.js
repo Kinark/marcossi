d74940b9d03884e8eee26b395e3aecc5ad172ddc
Kinark <igormarcossi@gmail.com>
2019-02-17 09:24:02 -0300

    Added SectionTitle component

---
+++ src/components/SectionTitle/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Highlight from '~/components/Highlight'
+
+// import styles from './styles.scss';
+
+const SectionTitle = ({ title, subtitle }) => (
+   <div className="section padded titles-color">
+      <h2 className="weight-medium">{title}</h2>
+      <Highlight><h3>{subtitle}</h3></Highlight>
+   </div>
+)
+
+SectionTitle.propTypes = {
+   title: PropTypes.string.isRequired,
+   subtitle: PropTypes.string.isRequired
+}
+
+export default SectionTitle
