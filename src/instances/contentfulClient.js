6b654bd17b92dfe9e674bd14b26a1c7ef0199587
Kinark <igormarcossi@gmail.com>
2019-02-17 06:12:07 -0300

    Adding contentfulClient instance

---
+++ src/instances/contentfulClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'contentful';
+
+export default createClient({
+   space: 'huyfk325as1q',
+   accessToken: 'c275c159e7bacc0e8a06c132ca4e3532ce2f4cd174c9f3979c7dceab4b238d7f'
+})
