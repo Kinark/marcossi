d0f9e52d03073bc14d7ac3b740e9d1324b1efcfa
Kinark <igormarcossi@gmail.com>
2019-02-17 06:12:51 -0300

    Added Home and Story views

---
+++ src/views/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Logo from './components/Logo'
+
+import styles from './styles.scss';
+
+const Home = props => (
+   <div>
+      Home
+      <Logo />
+   </div>
+)
+
+Home.propTypes = {
+
+}
+
+export default Home
---
+++ src/views/Story/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.scss';
+
+const Story = props => (
+   <div>
+      Story
+   </div>
+)
+
+Story.propTypes = {
+
+}
+
+export default Story
