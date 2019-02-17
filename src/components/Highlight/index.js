d1ec870255c9bcef64dc7e4c75096970d04e947d
Kinark <igormarcossi@gmail.com>
2019-02-17 09:25:04 -0300

    Fixed display and color issues with Hilighted subtitles

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
-const Highlight = ({ children }) => <div className={styles.highlighted}>{children}</div>
+const Highlight = ({ children }) => <div className="center titles-color"><div className={styles.highlighted}>{children}</div></div>
 
 Highlight.propTypes = {
    children: PropTypes.oneOfType([
