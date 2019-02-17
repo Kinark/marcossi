b614fffdd78ca9bf780d463fa7bd571a53cefd33
Kinark <igormarcossi@gmail.com>
2019-02-17 09:23:26 -0300

    Removed useless div

--- src/views/Home/components/Letter/index.js
+++ src/views/Home/components/Letter/index.js
@@ -4,22 +4,20 @@ import renderHTML from 'react-render-html';
 import { AppContext } from '~/instances/context';
 
 import LetterSvg from './images/Letter.svg'
-import styles from './styles.scss';
+// import styles from './styles.scss';
 
 const Letter = () => (
    <AppContext.Consumer>
       {({ data }) => (
-         <div>
-            <div className="container">
-               <div className="row xs-middle">
-                  <div className="col xs12 l7 xl6">
-                     <h2 className="weight-medium titles-color">{data.letter.title}</h2>
-                     <div className="weight-light long-text">{renderHTML(data.letter.text)}</div>
-                     <div className="right-align">{data.letter.credits}</div>
-                  </div>
-                  <div className="col xs12 l5 xl6 xs-first l-last center">
-                     <img src={LetterSvg} alt="A Letter" />
-                  </div>
+         <div className="container">
+            <div className="row xs-middle">
+               <div className="col xs12 l7 xl6">
+                  <h2 className="weight-medium titles-color">{data.letter.title}</h2>
+                  <div className="weight-light long-text">{renderHTML(data.letter.text)}</div>
+                  <div className="right-align">{data.letter.credits}</div>
+               </div>
+               <div className="col xs12 l5 xl6 xs-first l-last center">
+                  <img src={LetterSvg} alt="A Letter" />
                </div>
             </div>
          </div>
