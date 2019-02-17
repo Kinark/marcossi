8b3919df6c4baeaaff669a1840a9749c5dc3fe3e
Kinark <igormarcossi@gmail.com>
2019-02-17 08:29:05 -0300

    Added container, titles-color class and removed useless props param

--- src/views/Home/components/Logo/index.js
+++ src/views/Home/components/Logo/index.js
@@ -5,11 +5,13 @@ import Highlight from '~/components/Highlight'
 import logoSvg from './images/Logo.svg'
 import styles from './styles.scss';
 
-const Logo = props => (
-   <div className={styles.logoSection}>
-      <img src={logoSvg} alt="Marcossi" />
-      <h1 className="weight-medium">Marcossi</h1>
-      <Highlight><h3>Design as a story</h3></Highlight>
+const Logo = () => (
+   <div className={`${styles.logoSection} titles-color`}>
+      <div className="container">
+         <img src={logoSvg} alt="Marcossi" />
+         <h1 className="weight-medium">Marcossi</h1>
+         <Highlight><h3>Design as a story</h3></Highlight>
+      </div>
    </div>
 )
 
