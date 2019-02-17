3f50d2ef1fdd7323e7d953c0581aea9cbf4d89cc
Kinark <igormarcossi@gmail.com>
2019-02-17 09:27:25 -0300

    Finished (for now) features section

--- src/views/Home/components/Features/index.js
+++ src/views/Home/components/Features/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { AppContext } from '~/instances/context';
+
+import SectionTitle from '~/components/SectionTitle'
+import Highlight from '~/components/Highlight'
+
+
+import World from './images/World.svg'
+import Artboard from './images/Artboard.svg'
+import Toolbox from './images/Toolbox.svg'
+
 import styles from './styles.scss';
 
-const Features = props => (
-   <div>
-      Features
-   </div>
+const Features = () => (
+   <AppContext.Consumer>
+      {({ data }) => (
+         <div className="container center">
+            <SectionTitle title={data.features.title} subtitle={data.features.subtitle} />
+            <div className="row xs-row">
+               <Feature img={World} title={data.features.site.title} description={data.features.site.subtitle}/>
+               <Feature img={Artboard} title={data.features.branding.title} description={data.features.branding.subtitle}/>
+               <Feature img={Toolbox} title={data.features.apps.title} description={data.features.apps.subtitle}/>
+            </div>
+         </div>
+      )}
+   </AppContext.Consumer>
 )
 
 Features.propTypes = {
@@ -14,3 +33,17 @@ Features.propTypes = {
 }
 
 export default Features
+
+const Feature = ({ img, title, description }) => (
+   <div className="col xs12 m4">
+      <div className={styles.featureImgContainer}><img className="block" src={img} alt={title} /></div>
+      <Highlight><h3 className="weight-medium">{title}</h3></Highlight>
+      <p>{description}</p>
+   </div>
+)
+
+Feature.propTypes = {
+   img: PropTypes.string.isRequired,
+   title: PropTypes.string.isRequired,
+   description: PropTypes.string.isRequired
+}
