6794273cb3570119fa33302e51713eb042a8a6bc
Kinark <igormarcossi@gmail.com>
2019-03-03 21:58:30 -0300

    Twisting my face all the time

--- src/views/Home/components/Introduction/index.js
+++ src/views/Home/components/Introduction/index.js
@@ -3,6 +3,8 @@ import renderHTML from 'react-render-html';
 
 import { AppContext } from '~/instances/context';
 
+import { Twists } from '~/components/Twists';
+
 import Igor from './images/Igor.svg'
 import styles from './styles.scss';
 
@@ -13,7 +15,7 @@ const Introduction = () => (
             <div className="container">
                <div className="row">
                   <div className="col xs12 l5 xl4 offset-xl1 center">
-                     <img className={styles.igorIllustration} src={Igor} alt="Igor's Face" />
+                     <Twists><img className={styles.igorIllustration} src={Igor} alt="Igor's Face" /></Twists>
                   </div>
                   <div className="col xs12 l7 xl6">
                      <h2 className="weight-medium titles-color">{renderHTML(data.introduction.title)}</h2>
