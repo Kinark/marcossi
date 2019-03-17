dc4c194567fbe32472823f380bd28b6ed302157c
Kinark <igormarcossi@gmail.com>
2019-03-17 09:07:33 -0300

    Added custom className to Twist components

--- src/components/Twists/index.js
+++ src/components/Twists/index.js
@@ -3,14 +3,23 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
-export const Twists = ({ children }) => <div className={styles.twists}>{children}</div>
-export const TwistsOnHover = ({ children }) => <div className={styles.twistsOnHover}>{children}</div>
-export const TwistsForeverOnHover = ({ children }) => <div className={styles.twistsForeverOnHover}>{children}</div>
+export const Twists = ({ children, className }) => <div className={`${styles.twists} ${className}`}>{children}</div>
+export const TwistsOnHover = ({ children, className }) => <div className={`${styles.twistsOnHover} ${className}`}>{children}</div>
+export const TwistsForeverOnHover = ({ children, className }) => <div className={`${styles.twistsForeverOnHover} ${className}`}>{children}</div>
 
 const commonPropTypes = {
    children: PropTypes.element.isRequired,
+   className: PropTypes.string
+}
+
+const commonDefaultProps = {
+   className: ''
 }
 
 Twists.propTypes = commonPropTypes
 TwistsOnHover.propTypes = commonPropTypes
 TwistsForeverOnHover.propTypes = commonPropTypes
+
+Twists.defaultProps = commonDefaultProps
+TwistsOnHover.defaultProps = commonDefaultProps
+TwistsForeverOnHover.defaultProps = commonDefaultProps
