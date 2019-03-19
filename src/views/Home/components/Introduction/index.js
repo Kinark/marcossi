9470e73d403b904b810bf2b784b20c9c61b4728c
Kinark <igormarcossi@gmail.com>
2019-03-19 18:49:46 -0300

    Replaced Twists by TwistsOnHover

--- src/views/Home/components/Introduction/index.js
+++ src/views/Home/components/Introduction/index.js
@@ -3,7 +3,7 @@ import renderHTML from 'react-render-html';
 
 import { AppContext } from '~/instances/context';
 
-import { Twists } from '~/components/Twists';
+import { TwistsOnHover } from '~/components/Twists';
 
 import Igor from './images/Igor.svg'
 import styles from './styles.scss';
@@ -15,7 +15,7 @@ const Introduction = () => (
             <div className="container">
                <div className="row">
                   <div className="col xs12 l5 xl4 offset-xl1 center">
-                     <Twists><img className={styles.igorIllustration} src={Igor} alt="Igor's Face" /></Twists>
+                     <TwistsOnHover><img className={styles.igorIllustration} src={Igor} alt="Igor's Face" /></TwistsOnHover>
                   </div>
                   <div className="col xs12 l7 xl6">
                      <h2 className="weight-medium titles-color">{renderHTML(data.introduction.title)}</h2>
