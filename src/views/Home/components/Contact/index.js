68130ad1a25ace3d978d192dbf59c97d5def828b
Kinark <igormarcossi@gmail.com>
2019-03-21 09:04:04 -0300

    Fixed wrong imports

--- src/views/Home/components/Contact/index.js
+++ src/views/Home/components/Contact/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
-let cx = classNames.bind(styles);
-
 export default class Contact extends React.Component {
    static propTypes = {
 
