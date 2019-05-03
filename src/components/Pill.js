5adb1b1a530930e1f258afff3ba8c13c70abc759
Kinark <igormarcossi@gmail.com>
2019-05-03 16:51:15 -0300

    Created Pill component

---
+++ src/components/Pill.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.div`
+   padding: 8px 25px;
+   background-color: #F6F7FF;
+   border-radius: 100px;
+   box-shadow: 0px 10px 23px 0px rgba(32, 33, 44, 0.1);
+   transition: background-color 300ms ease-out;
+   &:hover {
+      background-color: #fafafa;
+   }
+`
