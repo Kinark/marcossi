5b807bb3e096d2ee4366d0a2b23dc510e31d3cb0
Kinark <igormarcossi@gmail.com>
2019-05-03 20:15:24 -0300

    Contact area's done

--- src/views/Home/components/Contact/index.js
+++ src/views/Home/components/Contact/index.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import classNames from 'classnames/bind';
-import PropTypes from 'prop-types';
+import React from 'react'
 
-export default class Contact extends React.Component {
-   static propTypes = {
+import { AppContext } from '~/instances/context'
 
-   }
+import SectionTitle from '~/components/SectionTitle'
+import Input from '~/components/Input'
+import Textarea from '~/components/Textarea'
+import Pill from '~/components/Pill'
 
-   state = {
-      value: null,
-   }
+import letter from './images/Letter.svg'
 
-   componentDidMount = () => {
-   }
-
-   componentWillUnmount = () => {
-   }
-
+export default class Contact extends React.PureComponent {
    render() {
       return (
-         <div>
-            Contact
-         </div>
-      );
+         <AppContext.Consumer>
+            {({ data }) => (
+               <div className="section padded container">
+                  <div className="center">
+                     <img src={letter} height="117" alt="Contact" />
+                  </div>
+                  <SectionTitle title={data.contact.title} subtitle={data.contact.subtitle} />
+                  <form name="contact" method="post">
+                     <div className="row">
+                        <div className="col xs12 m4">
+                           <Input type="text" name="name">
+                              {data.contact.inputs.name}
+                           </Input>
+                        </div>
+                        <div className="col xs12 m4">
+                           <Input type="email" name="email">
+                              {data.contact.inputs.email}
+                           </Input>
+                        </div>
+                        <div className="col xs12 m4">
+                           <Input type="text" name="phone">
+                              {data.contact.inputs.phone}
+                           </Input>
+                        </div>
+                     </div>
+                     <div className="row">
+                        <div className="col xs12">
+                           <Textarea type="text" name="message">
+                              {data.contact.inputs.message}
+                           </Textarea>
+                        </div>
+                     </div>
+                     <div className="right-align">
+                        <Pill type="submit">{data.contact.inputs.send}</Pill>
+                     </div>
+                  </form>
+               </div>
+            )}
+         </AppContext.Consumer>
+      )
    }
 }
