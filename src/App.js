d410b2e874d05ebd357f37cfacfdff4f40d9f4d3
Kinark <igormarcossi@gmail.com>
2019-05-02 22:32:33 -0300

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import { Route, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 //
 // ─── SERVICES ───────────────────────────────────────────────────────────────────
