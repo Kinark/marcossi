fb54cd4d4878c797a3154d6d0917f3c72a040d90
Kinark <igormarcossi@gmail.com>
2019-05-03 17:09:27 -0300

    Changed Pill's base component and tweakes styles

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
-export default styled.div`
+export default styled.button`
    padding: 8px 25px;
-   background-color: #F6F7FF;
+   background-color: #f6f7ff;
    border-radius: 100px;
    box-shadow: 0px 10px 23px 0px rgba(32, 33, 44, 0.1);
    transition: background-color 300ms ease-out;
+   border: none;
+   color: inherit;
+   cursor: pointer;
+   outline: none;
    &:hover {
       background-color: #fafafa;
    }
