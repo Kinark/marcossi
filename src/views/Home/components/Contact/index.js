9a056df88237d345b70933da3bf0c3784adbdc02
Kinark <igormarcossi@gmail.com>
2019-05-03 21:00:55 -0300

    Reduced Contact width

--- src/views/Home/components/Contact/index.js
+++ src/views/Home/components/Contact/index.js
@@ -14,40 +14,44 @@ export default class Contact extends React.PureComponent {
       return (
          <AppContext.Consumer>
             {({ data }) => (
-               <div className="section padded container">
-                  <div className="center">
-                     <img src={letter} height="117" alt="Contact" />
+               <div className="row">
+                  <div className="col xs12 xl8 offset-xl2">
+                     <div className="section padded container">
+                        <div className="center">
+                           <img src={letter} height="117" alt="Contact" />
+                        </div>
+                        <SectionTitle title={data.contact.title} subtitle={data.contact.subtitle} />
+                        <form name="contact" method="post">
+                           <div className="row">
+                              <div className="col xs12 m4">
+                                 <Input type="text" name="name">
+                                    {data.contact.inputs.name}
+                                 </Input>
+                              </div>
+                              <div className="col xs12 m4">
+                                 <Input type="email" name="email">
+                                    {data.contact.inputs.email}
+                                 </Input>
+                              </div>
+                              <div className="col xs12 m4">
+                                 <Input type="text" name="phone">
+                                    {data.contact.inputs.phone}
+                                 </Input>
+                              </div>
+                           </div>
+                           <div className="row">
+                              <div className="col xs12">
+                                 <Textarea type="text" name="message">
+                                    {data.contact.inputs.message}
+                                 </Textarea>
+                              </div>
+                           </div>
+                           <div className="right-align">
+                              <Pill type="submit">{data.contact.inputs.send}</Pill>
+                           </div>
+                        </form>
+                     </div>
                   </div>
-                  <SectionTitle title={data.contact.title} subtitle={data.contact.subtitle} />
-                  <form name="contact" method="post">
-                     <div className="row">
-                        <div className="col xs12 m4">
-                           <Input type="text" name="name">
-                              {data.contact.inputs.name}
-                           </Input>
-                        </div>
-                        <div className="col xs12 m4">
-                           <Input type="email" name="email">
-                              {data.contact.inputs.email}
-                           </Input>
-                        </div>
-                        <div className="col xs12 m4">
-                           <Input type="text" name="phone">
-                              {data.contact.inputs.phone}
-                           </Input>
-                        </div>
-                     </div>
-                     <div className="row">
-                        <div className="col xs12">
-                           <Textarea type="text" name="message">
-                              {data.contact.inputs.message}
-                           </Textarea>
-                        </div>
-                     </div>
-                     <div className="right-align">
-                        <Pill type="submit">{data.contact.inputs.send}</Pill>
-                     </div>
-                  </form>
                </div>
             )}
          </AppContext.Consumer>
